15e9672340ec8ee3b7c16494250d2bcd6f159c01
Faizan <f24aalam@gmail.com>
2021-12-09 12:12:26 +0530

    Addded Navbar and Header

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,19 @@
 <template>
-  <div class="bg-red-900 h-10 text-7xl">
-  </div>
+  <nav class="bg-gray-800 text-white h-8 flex items-center justify-between py-14 px-14 fixed top-0 w-full">
+    <a href="#pagetop" class="text-3xl font-bold font-mont">
+      START TAILWIND
+    </a>
+    <div class="flex gap-8 font-bold font-mont">
+      <a href="#portfolio">PORTFOLIO</a>
+      <a href="#about">ABOUT</a>
+      <a href="#contact">CONTACT</a>
+    </div>
+  </nav>
+  <header class="h-full flex items-center justify-center bg-green-400">
+    <div class="flex flex-col gap-4 items-center text-white text-center">
+      <img src="~/assets/images/avataaars.svg" class="w-60">
+      <h1 class="text-5xl font-bold uppercase font-mont">Start Tailwind</h1>
+      <p class="text-2xl font-mont font-normal">Graphic Artist - Web Designer - Illustrator</p>
+    </div>
+  </header>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mont': ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
